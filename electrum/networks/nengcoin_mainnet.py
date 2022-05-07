4954ab5e062038cc7e10b10d1bbf476e885cb468
ShorelineCrypto <admin@shorelinecrypto.com>
2022-05-06 22:15:00 -0400

    nengcoin chain param

--- electrum/networks/nengcoin_mainnet.py
+++ electrum/networks/nengcoin_mainnet.py
@@ -23,9 +23,9 @@ class NengcoinMainnet(AbstractNet, StakeMixin):
     BASE_UNITS_LIST = ['NENG', 'mNENG', 'uNENG', 'swartz']
     TESTNET = False
 
-    WIF_PREFIX = 0x9a
-    ADDRTYPE_P2PKH = 63
-    ADDRTYPE_P2SH = 23
+    WIF_PREFIX = 0xb0
+    ADDRTYPE_P2PKH = 53
+    ADDRTYPE_P2SH = 5
     XPRV_HEADERS = {
         'standard': 0x0488ade4,
     }
@@ -34,9 +34,9 @@ class NengcoinMainnet(AbstractNet, StakeMixin):
         'standard': 0x0488b21e,
     }
     XPUB_HEADERS_INV = inv_dict(XPUB_HEADERS)
-    BIP44_COIN_TYPE = 921
+    BIP44_COIN_TYPE = 681
 
-    GENESIS = "000003bf95cf7875987b333cc8e49a7c1a83583e5f5039b9af9555a92cb29651"
+    GENESIS = "14683bb988bcb69c74276df315c8de108d990fcff07483d5f2a044a3b4a592d8"
 
     DEFAULT_PORTS = {'t': '10001', 's': '10002'}
     DEFAULT_SERVERS = read_json('servers/Nengcoin-Mainnet.json', {})
@@ -47,30 +47,26 @@ class NengcoinMainnet(AbstractNet, StakeMixin):
 
     COINBASE_MATURITY = 100
     COIN = 100000000
-    TOTAL_COIN_SUPPLY_LIMIT = 999999999999
+    TOTAL_COIN_SUPPLY_LIMIT = 84000000000
     SIGNED_MESSAGE_PREFIX = b"\x18Nengcoin Signed Message:\n"
 
     DECIMAL_POINT_DEFAULT = 8 # NENG
-    TARGET_SPACING = int(2 * 60)
-    DGW_FORK_BLOCK = 0
+    
+    TARGET_TIMESPAN = int(1 * 24 * 60 * 60)
+    TARGET_SPACING = int(1 * 60)
+    INTERVAL = int(TARGET_TIMESPAN / TARGET_SPACING)
 
     BLOCK_EXPLORERS = {
-        'explorer.nengcoin.info': ('https://explorer.nengcoin.info', {'tx': '/tx/', 'addr': '/address/'}),
-        'openchains.info': ('https://openchains.info', {'tx': '/coin/nengcoin/tx/', 'addr': '/coin/nengcoin/address/'}),
+        'nengexplorer.mooo.com': ('http://nengexplorer.mooo.com:3001/', {'tx': '/tx/', 'addr': '/address/'}),
     }
-
-    @classmethod
-    def hash_raw_header(cls, header):
-        import algomodule
-        return hash_encode(algomodule._quark_hash(bfh(header)))
 
 
     @classmethod
     def get_target(cls, height: int, blockchain) -> int:
-        index = height // 2016 - 1
+        index = height // 1440 - 1
         if index == -1:
             return cls.MAX_TARGET
 
-        # Blockchain is PURE POS so we dont have the info needed to
+        # NENG Blockchain is randomSpike on top of scrypt so that we dont have the info needed to
         # calculate the targets required
         return 0
